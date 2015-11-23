2cf7f0aed3135e237ca38843cce570bf49a656be
Ramadhaniah Al Wardah <youmei.ts1@gmail.com>
2015-11-23 11:55:46 +0800

    Create Latihan5-15.php

---
+++ Ramadhaniah-Al-Wardah-1415015020/modul-5/Latihan5-15.php
@@ -0,0 +1,12 @@
+<?php
+	function familyName($fname) {
+		echo "$fname Refnes.<br>";
+	}
+	
+	familyName("Jani");
+	familyName("Hege");
+	familyName("Stale");
+	familyName("Kai Jim");
+	familyName("Borge");
+	# echo "BY THE WAY, ARE THOSE RANDOM NAMES?! I COULD USE THEM! Heh, sorry for stealing!";
+?>
