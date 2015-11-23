c405461c69a90f209faed802d22ab67bde46facf
Ramadhaniah Al Wardah <youmei.ts1@gmail.com>
2015-11-23 11:48:51 +0800

    Create Latihan5-3.php

---
+++ Ramadhaniah-Al-Wardah-1415015020/modul-5/Latihan5-3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Latihan5-3</title>
+	</head>
+	<body>
+		<?php
+			// Ini adalah komentar satu baris
+			
+			#  Ini juga komentar satu baris
+			
+			/* Ini adalah blok komentar
+			   berbaris jamak yang dapat
+			   mencakup beberapa baris
+			   sampai komentar ditutup --> */
+			
+			// Komentar juga dapat digunakan untuk "menghilangkan" sebagian dari sebuah kode
+			$x = 5 /* + 15 */ + 5;
+			echo $x; //Nanti yang muncul 10
+		?>
+	</body>
+</html>
