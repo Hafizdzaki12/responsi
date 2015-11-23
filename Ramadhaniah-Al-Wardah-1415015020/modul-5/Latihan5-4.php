f1fec7a7dd1ad05f90b79ee33da8f414f1f2ecff
Ramadhaniah Al Wardah <youmei.ts1@gmail.com>
2015-11-23 11:49:59 +0800

    Create Latihan5-4.php

---
+++ Ramadhaniah-Al-Wardah-1415015020/modul-5/Latihan5-4.php
@@ -0,0 +1,18 @@
+<!-- PHP Case Sensitive -->
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Latihan5-4</title>
+	</head>
+	<body>
+		<?php
+			$color = "red";
+			echo "My car is ".$color."<br>";
+			echo "My house is ".$COLOR."<br>";
+			echo "My boat is ".$coLOR."<br>";
+			
+			# Jadi, kesimpulannya PHP itu case-sensitive. Oke.
+		?>
+	</body>
+</html>
