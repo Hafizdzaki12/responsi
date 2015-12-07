6e14259bf07915fdf937ead2a9151111d63ae1a8
Ramadhaniah Al Wardah <youmei.ts1@gmail.com>
2015-12-07 15:49:35 +0800

    Create logout.php

---
+++ Ramadhaniah-Al-Wardah-1415015020/Modul-9/logout.php
@@ -0,0 +1,9 @@
+<!-- Logout -->
+
+<?php
+	session_start();
+	unset($_SESSION['login']);
+	unset($_SESSION['username']);
+	echo "Sukses logout. Silahkan <a href='login.php'>login</a> untuk masuk ke halaman admin.";
+	
+?>
