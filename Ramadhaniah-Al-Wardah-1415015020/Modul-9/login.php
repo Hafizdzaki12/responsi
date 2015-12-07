8269961df4690bf3663f459698727c709fbaa61a
Ramadhaniah Al Wardah <youmei.ts1@gmail.com>
2015-12-07 15:47:57 +0800

    Create login.php

---
+++ Ramadhaniah-Al-Wardah-1415015020/Modul-9/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!-- Form Login -->
+
+<html>
+	<head>
+		<title>Latihan9-a1</title>
+	</head>
+	<body>
+		<form action="proses-login.php" method="POST">
+			Username <input type="text" name="username" placeholder="Enter the username">
+			Password <input type="password" name="password" placeholder="Enter the password">
+			<input type="submit" value="Login">
+		</form>
+	</body>
+</html>
