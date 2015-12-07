71bc4cfe4bc538f655c8bb0c73a01f9191cda481
Raventail11 <jypbrn11@gmail.com>
2015-12-07 21:46:38 +0800

    Create tugas4.php

---
+++ JerryYosuaPadant/Modul-8/tugas4.php
@@ -0,0 +1,27 @@
+<!-- Memasukkan data pada tabel -->
+
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "myDB";
+	
+	//Membuat koneksi
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	
+	//Memeriksa koneksi
+	if (!$conn){ //jika tidak terkoneksi
+		die("Connection failed: ". mysqli_connect_error());
+	}
+	
+	//SQL untuk mengisi data
+	$sql = "INSERT INTO MyGuests (firstname, lastname, email) VALUES
+	('John', 'Doe', 'john@example.com')";
+	if (mysqli_query($conn, $sql)){
+		echo "New record created successfully.";
+	}
+	else {
+		echo "Error: ".$sql."<br>".mysqli_error($conn);
+	}
+	mysqli_close($conn);
+?>
